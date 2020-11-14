73a5489e825ba575afcd056df49fd1f167ee7275
Zoe Lapomme <zoe.lapomme@mail.mcgill.ca>
2020-11-14 14:00:56 -0500

    got the business card to render

--- frontend/codejam2020/src/Component/BusinessDisplay/BusinessCard.tsx
+++ frontend/codejam2020/src/Component/BusinessDisplay/BusinessCard.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import Paper from "@material-ui/core/Paper";
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
 
 interface Props {
   businessInfo: {
@@ -10,9 +16,37 @@ interface Props {
     type: string;
   };
 }
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
 const BusinessCard: React.FC<Props> = ({ businessInfo }) => {
+  const classes = useStyles();
+
   return (
-    <Paper style={{ width: "100%", height: "2rem" }}>{businessInfo.name}</Paper>
+    <Paper className={classes.root}>
+      <Typography className={classes.title} color="textSecondary" gutterBottom>
+        {businessInfo.type.toUpperCase().substring(0,1) + businessInfo.type.toLowerCase().substring(1)}
+      </Typography>
+      <Typography variant="h5" component="h2">
+        {businessInfo.name.toUpperCase().substring(0,1) + businessInfo.name.toLowerCase().substring(1)}    
+      </Typography>
+      <Typography className={classes.pos} color="textSecondary">
+          {businessInfo.location}
+      </Typography>
+      <Typography variant="body2" component="p">
+        {businessInfo.rating} / 5    
+      </Typography>
+    </Paper>
   );
 };
 
