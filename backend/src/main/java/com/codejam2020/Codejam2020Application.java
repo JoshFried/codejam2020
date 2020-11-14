0e37c40ef6a81b5a04e63b85724295c0ef9c84be
josh <josh.fried13@gmail.com>
2020-11-13 22:20:56 -0500

    init commit

---
+++ backend/src/main/java/com/codejam2020/Codejam2020Application.java
@@ -0,0 +1,13 @@
+package com.codejam2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Codejam2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Codejam2020Application.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/codejam2020/Codejam2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codejam2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Codejam2020ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
